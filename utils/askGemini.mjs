ba965af412b8be7e3c18392905bfc263503e60b3
Pony2Dis <pony2.dis@outlook.com>
2025-08-20 00:18:50 +0300

    incresed askGemini max output

--- utils/askGemini.mjs
+++ utils/askGemini.mjs
@@ -131,7 +131,7 @@ function buildGeminiBody({ cachedNames, deltaText, userPrompt }) {
     systemInstruction: { role: "user", parts: [{ text: SYSTEM_HE }] },
     cachedContents: cachedNames, // array of "cachedContents/..." IDs
     contents,
-    generationConfig: { temperature: 0.2, maxOutputTokens: 512 }
+    generationConfig: { temperature: 0.2, maxOutputTokens: 1024 }
   };
 }
 
